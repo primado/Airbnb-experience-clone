80193337e5522b84a5b1436b7ea6f852a1c92e41
primado <primadoprincepa@gmail.com>
2022-11-13 18:01:57 +0000

    added data file for props

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,39 @@ import React from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
+import data from "./data"
 import "./css/style.css"
 
 
 
 
 export default function App() {
+
+  const cards = data.map(item => {
+
+    return (
+      <Card
+          key={item.id}
+          {...item}
+          // img={item.coverImg}
+          // rating={item.stats.rating}
+          // reviewCount={item.stats.reviewCount}
+          // location={item.location}
+          // title={item.title}
+          // price={item.price}
+          // openSpots={item.openSpots}
+      />
+    )
+  })
+
   return (
     <div>
       <Navbar />
       <Hero />
-      <Card />
+      <section className="coverImg-flex">
+        {cards}
+      </section>
+     
     </div>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,26 +1,35 @@
 import React from "react";
 import Star from "../assets/Star-1.png"
-import Swimmer from "../assets/swimmer-12.png"
+import "../data"
 import "../css/style.css"
 
 
-export default function Card() {
+export default function Card(props) {
+
+    let badgeText
+    if (props.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.location === "Online") {
+        badgeText = "Online"
+    }
+
     return (
         <div>
             <section className="card-section">
                 <div className="cards">
                     <div className="card-one">
-                        <img src={Swimmer} alt="swimmer image" className="swimmer-img" />
+                    {badgeText && <div className="card--badge">{badgeText}</div>}
+                        <img src={`/images/${props.coverImg}`} alt="swimmer image" className="swimmer-img" />
                         <div className="card-content">
                             <div className="card-status">
                                 <img src={Star} alt="Star image" className="star-img"/>
-                                <span className="grey">5.0</span>
-                                <span className="grey">(6) • </span>
-                                <span className="grey"> USA</span>
+                                <span className="grey">{props.stats.rating}</span>
+                                <span className="grey">({props.stats.reviewCount}) • </span>
+                                <span className="grey"> {props.location}</span>
                             </div>
                             <div className="other-content">
-                                <p className="card-desc">Life lessons with Katie Zaferes</p>
-                                <p className="card-price"><span className="bold">From $136</span> / person</p>                         </div>
+                                <p className="card-desc">{props.title}</p>
+                                <p className="card-price"><span className="bold">From ${props.price}</span> / person</p>                         </div>
                         </div>
                     </div>
                 </div>
